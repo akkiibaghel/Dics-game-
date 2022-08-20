87f06859365033b2786c51b8758eb3d499b2d369
ABHISHEK KUMAR <78287129+akkiibaghel@users.noreply.github.com>
2022-08-20 12:11:07 +0530

    Update game_code.py

--- game_code.py
+++ game_code.py
@@ -8,22 +8,22 @@ while rolling_dics == "yes" or "y" or "Yes" or "YES" :
   name = input("Enter your name ")
   print("Roling the dics..... ")
   
-  print("The value are ....")
+  print("The value are ....\n")
   print()
   n=random.randint(min_val , max_val)
   m=random.randint(min_val , max_val)
-  print(f'This value of {name} : {n}')
-  print()
-  print(f'This value of computer:{m}')
-  print()
+  print(f'This value of {name} : {n}\n')
+  
+  print(f'This value of computer:{m}\n')
+  
   if n<m :
     print(f'kya yrr {name} tum phir haar gye ')
   else:
-    print("you are win! ..")
-  print()
+    print("you are win! ..\n")
   
-  print(name  + " " " VISHUDH CHUTIYA LADKE HO TUM ")
-  print()
+  
+  print(name  + " " " VISHUDH CHUTIYA LADKE HO TUM \n")
+
   
 
   rolling_again = input("do you want rolling again...")
